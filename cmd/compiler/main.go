eefb47088a6e63561d5086c5138e97ba22ae33f2
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2026-09-02 19:56:06 -0300

    fix: update input expression in main and refactor parser to use token types

--- cmd/compiler/main.go
+++ cmd/compiler/main.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"simple_compiler/parser"
-)
+import "simple_compiler/parser"
 
 func main() {
-	input := "8+5-7+9"
+	input := "89+508-7+99"
 
 	p := parser.NewParser([]byte(input))
 
--- parser/parser.go
+++ parser/parser.go
@@ -2,14 +2,14 @@ package parser
 
 import (
 	"fmt"
-	"unicode"
 
 	"simple_compiler/scanner"
+	"simple_compiler/token"
 )
 
 type Parser struct {
 	scan         *scanner.Scanner
-	currentToken byte
+	currentToken token.Token
 }
 
 func NewParser(input []byte) *Parser {
@@ -21,47 +21,43 @@ func NewParser(input []byte) *Parser {
 	}
 }
 
-func (p *Parser) nextToken() {
-	p.currentToken = p.scan.NextToken()
-}
-
 func (p *Parser) Parse() {
 	p.expr()
 }
 
 func (p *Parser) expr() {
-	p.digit()
+	p.number()
 	p.oper()
 }
 
-func (p *Parser) match(t byte) {
-	if p.currentToken == t {
+func (p *Parser) number() {
+	fmt.Println("push", p.currentToken.Lexeme)
+	p.match(token.NUMBER)
+}
+
+func (p *Parser) oper() {
+	switch p.currentToken.Type {
+	case token.PLUS:
+		p.match(token.PLUS)
+		p.number()
+		fmt.Println("add")
+		p.oper()
+	case token.MINUS:
+		p.match(token.MINUS)
+		p.number()
+		fmt.Println("sub")
+		p.oper()
+	}
+}
+
+func (p *Parser) match(t token.TokenType) {
+	if p.currentToken.Type == t {
 		p.nextToken()
 	} else {
 		panic("syntax error")
 	}
 }
 
-func (p *Parser) digit() {
-	if unicode.IsDigit(rune(p.currentToken)) {
-		fmt.Println("push", string(p.currentToken))
-		p.match(p.currentToken)
-	} else {
-		panic("syntax error")
-	}
-}
-
-func (p *Parser) oper() {
-	switch p.currentToken {
-	case '+':
-		p.match('+')
-		p.digit()
-		fmt.Println("add")
-		p.oper()
-	case '-':
-		p.match('-')
-		p.digit()
-		fmt.Println("sub")
-		p.oper()
-	}
+func (p *Parser) nextToken() {
+	p.currentToken = p.scan.NextToken()
 }
